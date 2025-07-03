1e9596bc6ae00ec790060d9b54bb07bcb052a18f
leoslopez <llopez@makingsense.com>
2025-07-03 13:53:00 -0300

    feat: add method to obtain Visitor Info

---
+++ Doppler.PushContact.Models/DTOs/VisitorInfoDTO.cs
@@ -0,0 +1,9 @@
+namespace Doppler.PushContact.Models.DTOs
+{
+    public class VisitorInfoDTO
+    {
+        public string Domain { get; set; }
+        public string Email { get; set; }
+        public string VisitorGuid { get; set; }
+    }
+}
