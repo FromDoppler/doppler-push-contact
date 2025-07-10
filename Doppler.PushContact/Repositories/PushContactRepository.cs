f672e57bb60de588cd5c9e77ae2bd784a339744d
leoslopez <llopez@makingsense.com>
2025-07-10 12:39:45 -0300

    feat: add method to obtain subscriptions info using cursor

--- Doppler.PushContact/Repositories/PushContactRepository.cs
+++ Doppler.PushContact/Repositories/PushContactRepository.cs
@@ -10,6 +10,8 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Doppler.PushContact.Repositories
@@ -61,6 +63,110 @@ namespace Doppler.PushContact.Repositories
 
                 throw;
             }
+        }
+
+        public async IAsyncEnumerable<SubscriptionInfoDTO> GetSubscriptionInfoByDomainAsStreamAsync(
+        string domain,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default
+        )
+        {
+            if (string.IsNullOrEmpty(domain))
+            {
+                throw new ArgumentException($"'{nameof(domain)}' cannot be null or empty.", nameof(domain));
+            }
+
+            var sizeFromConfig = _pushMongoContextSettings?.Value?.CursorBatchSize ?? 0;
+            var batchSize = sizeFromConfig > 0 ? sizeFromConfig : 500;
+
+            var filter = Builders<BsonDocument>.Filter.Eq(PushContactDocumentProps.DomainPropName, domain) &
+                         Builders<BsonDocument>.Filter.Eq(PushContactDocumentProps.DeletedPropName, false);
+
+            var options = new FindOptions<BsonDocument>
+            {
+                Projection = Builders<BsonDocument>.Projection
+                    .Include(PushContactDocumentProps.DeviceTokenPropName)
+                    .Include(PushContactDocumentProps.Subscription_PropName)
+                    .Include(PushContactDocumentProps.IdPropName),
+                BatchSize = batchSize // numbers of documents readed in each cursor page
+            };
+
+            // define cursor
+            using var cursor = await PushContacts.FindAsync(filter, options, cancellationToken);
+
+            var index = 0;
+            while (await cursor.MoveNextAsync(cancellationToken)) // read next page
+            {
+                index++;
+                _logger.LogDebug("number of page read from DB: {PageIndex}", index);
+                foreach (var doc in cursor.Current) // iterate docs of current page
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        _logger.LogWarning("Streaming cancelled while retrieving subscription info for domain: {Domain}", domain);
+                        yield break;
+                    }
+
+                    // take advantage of IAsyncEnumerable<> to return one item at a time (streaming by one)
+                    yield return GetSubscriptionInfoFromBson(doc);
+                }
+            }
+        }
+
+        private SubscriptionInfoDTO GetSubscriptionInfoFromBson(BsonDocument doc)
+        {
+            var DEVTOKEN_PROP_NAME = PushContactDocumentProps.DeviceTokenPropName;
+            var SUBSCRIPTION_PROP_NAME = PushContactDocumentProps.Subscription_PropName;
+            var ENDPOINT_PROP_NAME = PushContactDocumentProps.Subscription_EndPoint_PropName;
+            var AUTH_PROP_NAME = PushContactDocumentProps.Subscription_Auth_PropName;
+            var P256DH_PROP_NAME = PushContactDocumentProps.Subscription_P256DH_PropName;
+            var ID_PROP_NAME = PushContactDocumentProps.IdPropName;
+
+            var deviceToken = doc.Contains(DEVTOKEN_PROP_NAME) && !doc[DEVTOKEN_PROP_NAME].IsBsonNull
+                ? doc[DEVTOKEN_PROP_NAME].AsString
+                : null;
+
+            var pushContactId = doc.Contains(ID_PROP_NAME) && !doc[ID_PROP_NAME].IsBsonNull
+                ? doc[ID_PROP_NAME].ToString()
+                : null;
+
+            SubscriptionDTO subscriptionModel = null;
+
+            if (doc.Contains(SUBSCRIPTION_PROP_NAME) && !doc[SUBSCRIPTION_PROP_NAME].IsBsonNull)
+            {
+                var subscriptionDoc = doc[SUBSCRIPTION_PROP_NAME].AsBsonDocument;
+
+                var endPoint = subscriptionDoc.Contains(ENDPOINT_PROP_NAME) && !subscriptionDoc[ENDPOINT_PROP_NAME].IsBsonNull
+                    ? subscriptionDoc[ENDPOINT_PROP_NAME].AsString
+                    : null;
+
+                var auth = subscriptionDoc.Contains(AUTH_PROP_NAME) && !subscriptionDoc[AUTH_PROP_NAME].IsBsonNull
+                    ? subscriptionDoc[AUTH_PROP_NAME].AsString
+                    : null;
+
+                var p256dh = subscriptionDoc.Contains(P256DH_PROP_NAME) && !subscriptionDoc[P256DH_PROP_NAME].IsBsonNull
+                    ? subscriptionDoc[P256DH_PROP_NAME].AsString
+                    : null;
+
+                if (!string.IsNullOrEmpty(endPoint) && !string.IsNullOrEmpty(auth) && !string.IsNullOrEmpty(p256dh))
+                {
+                    subscriptionModel = new SubscriptionDTO
+                    {
+                        EndPoint = endPoint,
+                        Keys = new SubscriptionKeys
+                        {
+                            Auth = auth,
+                            P256DH = p256dh
+                        }
+                    };
+                }
+            }
+
+            return new SubscriptionInfoDTO
+            {
+                DeviceToken = deviceToken,
+                Subscription = subscriptionModel,
+                PushContactId = pushContactId
+            };
         }
 
         private List<SubscriptionInfoDTO> GetSubscriptionsInfoFromBsonDocuments(List<BsonDocument> pushContacts)
--- Doppler.PushContact/Services/PushMongoContextSettings.cs
+++ Doppler.PushContact/Services/PushMongoContextSettings.cs
@@ -14,5 +14,6 @@ namespace Doppler.PushContact.Services
 
         public string MessagesCollectionName { get; set; }
         public string WebPushEventCollectionName { get; set; }
+        public int CursorBatchSize { get; set; }
     }
 }
