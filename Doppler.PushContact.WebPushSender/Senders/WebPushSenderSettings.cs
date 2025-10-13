0ca31069a27b73cf92e21206a91358e8b6cdff32
leoslopez <llopez@makingsense.com>
2025-10-13 17:27:23 -0300

    feat: consider click link in action and ActionEventEndpoints array

--- Doppler.PushContact.WebPushSender/Senders/WebPushSenderSettings.cs
+++ Doppler.PushContact.WebPushSender/Senders/WebPushSenderSettings.cs
@@ -5,5 +5,7 @@ namespace Doppler.PushContact.WebPushSender.Senders
         public string QueueName { get; set; }
         public WebPushSenderTypes Type { get; set; }
         public string PushApiUrl { get; set; }
+        public string ActionClickEventEndpointPath { get; set; }
+        public string PushContactApiUrl { get; set; }
     }
 }
